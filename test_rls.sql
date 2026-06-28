ebc4e56b0ed5bae0f4c7d435180d2f9d2980efcc
Vignesh R <124500902+adamregi@users.noreply.github.com>
2026-06-28 20:21:02 +0530

    Delete test_rls.sql

--- test_rls.sql
+++
@@ -1,6 +0,0 @@
-SET ROLE authenticated;
-SELECT set_config('request.jwt.claims', '{"sub": "baaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"}', true);
-SELECT count(*) FROM public.get_executive_overview();
-
-SELECT set_config('request.jwt.claims', '{"sub": "bc52f3a9-4f79-4508-b50d-26b5d2aeb691"}', true);
-SELECT count(*) FROM public.get_executive_overview();
